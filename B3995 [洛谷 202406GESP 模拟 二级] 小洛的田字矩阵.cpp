27d946ec15f76fa2df04e45d45a798407b412fbc
JulieTang <hushanshan91@163.com>
2026-03-11 20:21:26 +0800

    Add files via upload

---
+++ B3995 [洛谷 202406GESP 模拟 二级] 小洛的田字矩阵.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	cout<<"|";
+	for(int j=0;j<n-2;j++)
+	{
+		cout<<"-"; 
+	}
+	cout<<"|";
+	cout<<endl;
+	for(int i=0;i<n/2-1;i++)
+	{
+		cout<<"|";
+		for(int j=0;j<n/2-1;j++)
+		{
+			cout<<"x";
+		}
+		cout<<"|";
+		for(int j=0;j<n/2-1;j++)
+		{
+			cout<<"x";
+		}
+		cout<<"|";
+		cout<<endl;
+	}	
+	cout<<"|";
+	for(int i=0;i<n/2-1;i++)
+	{
+		cout<<"-";
+	}
+	cout<<"x";
+	for(int i=0;i<n/2-1;i++)
+	{
+		cout<<"-";
+	}
+	cout<<"|";
+	cout<<endl;	
+	for(int i=0;i<n/2-1;i++)
+	{
+		cout<<"|";
+		for(int j=0;j<n/2-1;j++)
+		{
+			cout<<"x";
+		}
+		cout<<"|";
+		for(int j=0;j<n/2-1;j++)
+		{
+			cout<<"x";
+		}
+		cout<<"|";
+			cout<<endl;
+	}	
+	cout<<"|";
+	for(int j=0;j<n-2;j++)
+	{
+		cout<<"-"; 
+	}
+	cout<<"|";
+	return 0;
+}
